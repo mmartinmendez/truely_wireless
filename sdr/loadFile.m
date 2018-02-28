e2d3fd86e5db077f549fd9b64b45e23cc1c2d13c
Mithun <mithun.martin.215@gmail.com>
2018-02-28 12:20:02 +0100

    new dump file 1004

--- sdr/loadFile.m
+++ sdr/loadFile.m
@@ -6,13 +6,14 @@ function y = loadFile(filename)
 % SDR>rtl_sdr -s f_in_sps -f f_in_Hz -g gain_dB capture.bin
 % SDR>rtl_sdr -s 2400000 -f 70000000 -g 25 capture70R1k.bin
 
-fid = fopen('dump.bin');
+fid = fopen('dump_1001.bin');
 y = fread(fid,'uint8=>double');
 
 y = y-127;
 y = y(1:2:end) + i*y(2:2:end);
 
-% simpleSA(y,2^14,2400);
-simpleSA(y, 2^14, 2400,-100,0,'r');
-print -depsc -tiff capture.eps
+% plot_FFT_IQ(y,1,.002*2.4,100.4,100.400);
 
+% simpleSA(y,2^14,2400);
+simpleSA(y, 2^14, 2400,-80,50,'r');
+% print -depsc -tiff capture.eps
