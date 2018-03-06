42a99b2c5edf6f5f9482d683d2994e850ebf9983
Mithun <mithun.martin.215@gmail.com>
2018-03-06 10:45:05 +0100

    added plotting functions for the different scenerios

--- wireshark/main.py
+++ wireshark/main.py
@@ -1,4 +1,5 @@
 import pyshark
+import os, os.path
 import access_points as ap
 import netifaces as ni
 import matplotlib.pyplot as plt
@@ -24,7 +25,6 @@ def capture_packet(filename, timeout, interface, filter):
 
 def print_from_file(filename, filter):
     flt = 'wlan.ta == ' + str(filter)
-    print flt
     cap = pyshark.FileCapture(filename, display_filter=flt)
 
     i = 0
@@ -62,6 +62,23 @@ def get_access_point_mac(node_name):
             print "\n"
             return str(node['bssid'])
 
+def calculate_average(filename, filter):
+    flt = 'wlan.ta == ' + str(filter)
+    cap = pyshark.FileCapture(filename, display_filter=flt)
+    sum = 0
+    i = 0
+    for pkt in cap
+        snr = int(pkt.radiotap.dbm_antsignal)-int(pkt.radiotap.dbm_antnoise)
+        sum += snr
+        i += 1
+    average = sum/i
+    return average
+
+def get_snr_values(files):
+    snr = []
+    for i in range(n_files):
+        snr.append(calculate_average(files[i], flt))
+    return snr
 
 """ 
     Values compared between 5 distances
@@ -69,10 +86,23 @@ def get_access_point_mac(node_name):
     This data is then plotted against distance at which the packets were captured
     
     Run only after confirming if all the files needed are present 
+    20m, 15m, 10m, 5m, <1m
 """
-def scenerio_distance(no_files):
+def scenerio_distance(n_files):
     #TODO
+    DIR = './distances'
+    print len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+    files = {'dt1.pcap', 'dt2.pcap', 'dt3.pcap', 'dt4.pcap', 'dt5.pcap'}
+    distance = {1, 5, 10, 15, 20}
 
+    snr = get_snr_values(files)
+
+    plt.plot(snr,distance)
+    plt.title('SNR fluctuation with distance to the access point')
+    plt.xlabel('Distance')
+    plt.ylabel('SNR')
+    plt.savefig("distance_plot.pdf", bbox_inches='tight', pad_inches=0.5)
+    plt.close()
 
 """
     Investigate if interference causes degradation of signal strength
@@ -82,18 +112,56 @@ def scenerio_distance(no_files):
 """
 def scenerio_interference():
     #TODO
+    DIR = './interference'
+    print len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+    files = {'inter1.pcap', 'inter2.pcap', 'inter3.pcap'}
+    distance = {1,2,3,4,5}
+
+    snr = get_snr_values(files)
+
+    plt.plot(snr, distance)
+    plt.title('SNR fluctuation with distance from the interference point')
+    plt.xlabel('Distance')
+    plt.ylabel('SNR')
+    plt.savefig("interference_plot.pdf", bbox_inches='tight', pad_inches=0.5)
+    plt.close()
 
 """
     Investigate the difference the access point model contributes towards the signal strength
+    Take values at a distance of 5m from the access points
 """
 def scenerio_manufacturer():
     #TODO
+    DIR = './manufacturer'
+    print len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
+    files = {'tp_link.pcap', 'fritzbox.pcap'}
+    snr = get_snr_values(files)
+
+    plt.plot(snr)
+    plt.title('SNR fluctuation with different access points')
+    plt.xlabel('Manufacturer')
+    plt.ylabel('SNR')
+    plt.savefig("manufacturer_plot.pdf", bbox_inches='tight', pad_inches=0.5)
+    plt.close()
 
 """
     Investigate the improvement/degradation caused by channel hop
 """
 def scenerio_channel():
     #TODO
+    DIR = './channels'
+    print len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
+    files = {'channel1.pcap', 'channel2.pcap', 'channel3.pcap'}
+    snr = get_snr_values(files)
+
+    plt.plot(snr)
+    plt.title('SNR fluctuation with different channels')
+    plt.xlabel('Channel')
+    plt.ylabel('SNR')
+    plt.savefig("manufacturer_plot.pdf", bbox_inches='tight', pad_inches=0.5)
+    plt.close()
 
 main()
 
